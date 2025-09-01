ecd90fc80b25f70227bb7118170f104b27e5eb26
Jay Sorg <jay.sorg@gmail.com>
2025-08-31 23:27:52 -0700

    change to static build

--- build.zig
+++ build.zig
@@ -11,7 +11,7 @@ pub fn build(b: *std.Build) void
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
     // librdpsnd
-    const librdpsnd = b.addSharedLibrary(.{
+    const librdpsnd = b.addStaticLibrary(.{
         .name = "rdpsnd",
         .root_source_file = b.path("src/librdpsnd.zig"),
         .target = target,
